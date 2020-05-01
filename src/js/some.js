1471c6c49cba3916d32e97eb28c1ec33d33e62e2
andreysomov86 <49700885+andreysomov86@users.noreply.github.com>
2020-05-01 03:05:10 +0300

    Delete some.js

--- src/js/some.js
+++
@@ -1,9 +0,0 @@
-import $ from 'jquery';
-import divide from './lib.js';
-
-console.log(divide(4, 2));
-console.log(divide(6, 2));
-console.log(divide(8, 2));
-console.log(divide(100));
-
-$('p').addClass('green');
