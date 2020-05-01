4720b49653317a78b7ebe12e27a1ee3d7aaf50b4
andreysomov86 <49700885+andreysomov86@users.noreply.github.com>
2020-05-01 03:04:48 +0300

    Delete app.js

--- src/js/app.js
+++
@@ -1,6 +0,0 @@
-import $ from "jquery";
-import SomeMath from "./js/average.js";
-$('.title').html("some text!!!");
-console.log(SomeMath.avg(1,2,3));
-console.log(SomeMath.merge({a:1 }, {b: 2}));
-import './css/style.css';
